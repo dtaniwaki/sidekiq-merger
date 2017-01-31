24127d50e8da71b13e3f80600a1124e82a3b0491
dtaniwaki <daisuketaniwaki@gmail.com>
2017-01-31 10:00:05 +0900

    Update web UI

--- lib/sidekiq/merger/web.rb
+++ lib/sidekiq/merger/web.rb
@@ -5,13 +5,14 @@ module Sidekiq::Merger::Web
 
   def self.registered(app)
     app.get "/merger" do
-      @batches = Sidekiq::Merger::Batch.all
+      @merges = Sidekiq::Merger::Batch.all
       erb File.read(File.join(VIEWS, "index.html.erb")), locals: { view_path: VIEWS }
     end
 
-    app.post "/merger/*/delete" do |full_merge_key|
+    app.post "/merger/:full_merge_key/delete" do
+      full_merge_key = URI.decode_www_form_component params[:full_merge_key]
       batch = Sidekiq::Merger::Batch.initialize_with_full_merge_key(full_merge_key)
-      batch.delete
+      batch.delete_all
       redirect "#{root_path}/merger"
     end
   end
