0a76fc24af2a51951e31b924c8e8f622f1e61a59
dtaniwaki <daisuketaniwaki@gmail.com>
2017-02-01 10:44:22 +0900

    Rename sidekiq web tab endpoint

--- lib/sidekiq/merger/web.rb
+++ lib/sidekiq/merger/web.rb
@@ -4,19 +4,19 @@ module Sidekiq::Merger::Web
   VIEWS = File.expand_path("views", File.dirname(__FILE__))
 
   def self.registered(app)
-    app.get "/merger" do
+    app.get "/merges" do
       @merges = Sidekiq::Merger::Merge.all
       erb File.read(File.join(VIEWS, "index.erb")), locals: { view_path: VIEWS }
     end
 
-    app.post "/merger/:full_merge_key/delete" do
+    app.post "/merges/:full_merge_key/delete" do
       full_merge_key = URI.decode_www_form_component params[:full_merge_key]
       merge = Sidekiq::Merger::Merge.initialize_with_full_merge_key(full_merge_key)
       merge.delete_all
-      redirect "#{root_path}/merger"
+      redirect "#{root_path}/merges"
     end
   end
 end
 
 Sidekiq::Web.register(Sidekiq::Merger::Web)
-Sidekiq::Web.tabs["Merger"] = "merger"
+Sidekiq::Web.tabs["Merges"] = "merges"
