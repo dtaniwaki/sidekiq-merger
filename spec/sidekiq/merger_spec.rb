c6e154349347ecd85af5021479ed43e34b90fb51
dtaniwaki <daisuketaniwaki@gmail.com>
2017-02-06 13:15:01 +0900

    Fix typo in spec

--- spec/sidekiq/merger_spec.rb
+++ spec/sidekiq/merger_spec.rb
@@ -4,7 +4,7 @@ describe Sidekiq::Merger do
   it "has a version number" do
     expect(described_class::VERSION).not_to be nil
   end
-  describe "#create_task" do
+  describe ".create_task" do
     it "starts a monitoring task" do
       task = described_class.create_task
       expect(task).to be_a Concurrent::TimerTask
