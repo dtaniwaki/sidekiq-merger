33733871a111e951b30b96fc4f72b3d21fce53be
dtaniwaki <daisuketaniwaki@gmail.com>
2017-02-12 19:16:12 +0900

    Use audience's own queue

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,7 @@
 require_relative "./sidekiq"
 require "sinatra/base"
+require "sinatra/cookies"
+require "securerandom"
 require "rack/flash"
 require "sidekiq/web"
 require "sidekiq-status/web"
@@ -9,35 +11,57 @@ class App < Sinatra::Application
   enable :sessions
   use Rack::Flash
 
+  before do
+    @queue = cookies[:queue] ||= SecureRandom.urlsafe_base64(8)
+  end
+
   get "/" do
     erb :index
   end
 
   post "/some_worker/perform_in" do
     n = rand(10)
-    SomeWorker.perform_in((params[:in] || 60).to_i, n)
-    flash[:notice] = "Added #{n} to SomeWorker"
+    Sidekiq::Client.push(
+      "queue" => @queue,
+      "class" => SomeWorker,
+      "args" => [n],
+      "at" => Time.now + (params[:in] || 60)
+    )
+    flash[:notice] = "Added #{n} to SomeWorker to Queue #{@queue}"
     redirect "/"
   end
 
   post "/some_worker/perform_async" do
     n = rand(10)
-    SomeWorker.perform_async(n)
-    flash[:notice] = "Added #{n} to SomeWorker"
+    Sidekiq::Client.push(
+      "queue" => @queue,
+      "class" => SomeWorker,
+      "args" => [n]
+    )
+    flash[:notice] = "Added #{n} to SomeWorker to Queue #{@queue}"
     redirect "/"
   end
 
   post "/unique_worker/perform_in" do
     n = rand(10)
-    UniqueWorker.perform_in((params[:in] || 60).to_i, n)
-    flash[:notice] = "Added #{n} to UniqueWorker"
+    Sidekiq::Client.push(
+      "queue" => @queue,
+      "class" => UniqueWorker,
+      "args" => [n],
+      "at" => Time.now + (params[:in] || 60)
+    )
+    flash[:notice] = "Added #{n} to UniqueWorker to Queue #{@queue}"
     redirect "/"
   end
 
   post "/unique_worker/perform_async" do
     n = rand(10)
-    UniqueWorker.perform_async(n)
-    flash[:notice] = "Added #{n} to UniqueWorker"
+    Sidekiq::Client.push(
+      "queue" => @queue,
+      "class" => UniqueWorker,
+      "args" => [n]
+    )
+    flash[:notice] = "Added #{n} to UniqueWorker to Queue #{@queue}"
     redirect "/"
   end
 end
